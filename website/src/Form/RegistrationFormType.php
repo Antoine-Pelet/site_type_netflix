2f28eee069aa16a5f532cf99d605c6ad0b16f1f4
guimbeau <alex.guimbeau@etu.u-bordeaux.fr>
2023-01-10 15:33:48 +0100

    un vrai mail obligatoir pour s'inscrir

--- website/src/Form/RegistrationFormType.php
+++ website/src/Form/RegistrationFormType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -19,7 +20,7 @@ class RegistrationFormType extends AbstractType
     {
         $builder
             ->add('name')
-            ->add('email')
+            ->add('email', EmailType::class)
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
