f4c6fbbcc95d1a7030961f44beed55ddcb24246c
lecopeaux <lenny.copeaux@etu.u-bordeaux.fr>
2023-01-11 22:16:44 +0100

    Grosse modification au niveau du front

--- website/src/Controller/SeriesController.php
+++ website/src/Controller/SeriesController.php
@@ -128,7 +128,7 @@ class SeriesController extends AbstractController
     }
 
     #[Route('/series/like/list', name: 'app_liked_series', methods: ['GET'])]
-    public function listLikedSeries(): Response
+    public function listLikedSeries(Request $request, PaginatorInterface $paginator, EntityManagerInterface $entityManager): Response
     {
 
         /** @var \App\Entity\User */
@@ -136,8 +136,18 @@ class SeriesController extends AbstractController
 
         $likedSeries = $user->getSeries();
 
+         // Paginate the results of the query
+         $appointments = $paginator->paginate(
+            // Doctrine Query, not results
+            $likedSeries,
+            // Define the page parameter
+            $request->query->getInt('page', 1),
+            // Items per page
+            25
+        );
+
         return $this->render('liked/like.html.twig', [
-            'series' => $likedSeries,
+            'series' => $appointments,
         ]);
     }
 
