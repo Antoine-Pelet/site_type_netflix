aa52df97b994d5cd91bd18cc987a1c035cc7e3af
akerchaoui@ <akerchaoui@>
2023-01-12 11:57:09 +0100

    avancement de l'acceuil

--- website/src/Controller/DefaultController.php
+++ website/src/Controller/DefaultController.php
@@ -3,16 +3,29 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Series;
 
 class DefaultController extends AbstractController
 {
-    #[Route('/', name: 'app_default')]
-    public function index(): Response
+    #[Route('/', name: 'app_default', methods: ['GET'])]
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $appointmentsRepository = $entityManager->getRepository(Series::class);
+
+        $youtubeTrailer = $appointmentsRepository->createQueryBuilder('s')
+        ->select('s.title, s.youtubeTrailer')
+        ->orderby('RAND()')
+        ->getQuery()
+        ->getResult();
+
+
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
+            'series' => $youtubeTrailer
         ]);
     }
 }
